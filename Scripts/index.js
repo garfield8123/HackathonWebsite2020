f897be16fe60f23da86702a81964ddc5ac15431a
garfield8123 <garfield8123@github.com>
2019-05-12 10:52:54 -0700

    imageslideshow

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,14 @@
-$(function(){
-	$('.fadein img:gt(0)').hide();
-	setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 8000);
-});
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex+=2;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
