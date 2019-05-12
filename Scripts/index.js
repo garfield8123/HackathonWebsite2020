0cf8c7b437f22fc6b74f670043e364ab44aa1985
garfield8123 <garfield8123@github.com>
2019-05-11 22:30:21 -0700

    testslideshow

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,7 +3,7 @@ carousel();
 
 function carousel() {
   var i;
-  var x = document.getElementsByClassName("myslides");
+  var x = document.getElementsByClassName("mySlides");
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
