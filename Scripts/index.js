8f1483977c537897cd3d9c008ecf24ea1bb0c428
garfield8123 <garfield8123@github.com>
2019-05-11 22:55:45 -0700

    slideshow

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,8 +1,4 @@
 $(function(){
-    $('.fadein img:gt(0)').hide();
-    setInterval(function(){
-      $('.fadein :first-child').fadeOut()
-         .next('img').fadeIn()
-         .end().appendTo('.fadein');}, 
-      2000);
+	$('.fadein img:gt(0)').hide();
+	setInterval(function(){$('.fadein :first-child').fadeOut().next('img').fadeIn().end().appendTo('.fadein');}, 3000);
 });
