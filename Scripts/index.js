a02c63682144a050d1adc2d971e221026a59c5ed
garfield8123 <garfield8123@github.com>
2019-05-21 08:33:29 -0700

    fixslideshow

--- Scripts/index.js
+++ Scripts/index.js
@@ -4,11 +4,18 @@ carousel();
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+  i = 0;
+  while(i < x.length){
+    x[i].style.display = "none"
+    i +=2;
   }
-  myIndex+=3;
+  /*
+  for (i = 0; i < x.length; i+=2) {
+    x[i].style.display = "none";  
+  }*/
+  myIndex+=2;
   if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
+  x[myIndex-1].style.display = "block";
+  
   setTimeout(carousel, 5000); // Change image every 2 seconds
 }
