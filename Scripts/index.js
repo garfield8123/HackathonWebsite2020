84efe71937eef7913b9ce2f7c6b69c98dcea9a04
garfield8123 <34069094+garfield8123@users.noreply.github.com>
2020-01-07 23:57:57 -0800

    logger

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,7 +1,3 @@
 function logclick(){
-  const fs = require('fs')
-  let data = "Someone clicked the link"
-  fs.writeFile('logger.txt', data, (err) => {
-    if(err) throw err;
-  })
+  $('a[target="_blank"]').removeAttr('target');
 }
